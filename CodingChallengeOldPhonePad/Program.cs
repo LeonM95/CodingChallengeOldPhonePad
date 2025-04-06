9ed2e8ad3c7e62c5eff80c6fccade45ba5cb2b2b
LeonM95 <leomasis95@gmail.com>
2025-04-05 18:19:04 -0600

    Upload coding challenge

---
+++ CodingChallengeOldPhonePad/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallengeOldPhonePad
+{
+    internal class Program
+    {
+        // main function with test cases
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OldPhonePad("33#"));  // Output should be "E"
+            Console.WriteLine(OldPhonePad("227*#"));  // Output should be "B"
+            Console.WriteLine(OldPhonePad("4433555 555666#"));  // Output should be "HELLO"
+            Console.WriteLine(OldPhonePad("8 88777444666*664#"));  // Output should be ?????
+        }
+
+        public static string OldPhonePad(string input)
+        {
+            string msg = ""; //variable to store the message 
+            string currValue = ""; //store current value in the loop 
+
+            //use Dictionary to store phone keypad 
+            Dictionary<string, string> keypad = new Dictionary<string, string>();
+            keypad.Add("2", "abc");
+            keypad.Add("3", "def");
+            keypad.Add("4", "ghi");
+            keypad.Add("5", "jkl");
+            keypad.Add("6", "mno");
+            keypad.Add("7", "pqrs");
+            keypad.Add("8", "tuv");
+            keypad.Add("9", "wxyz");
+
+            for (int i = 0; i < input.Length; i++) //iterates the input given
+            {
+                char key = input[i]; //get one character
+
+                if (key == ' ') // if there is a pause
+                {
+                    if (currValue.Length > 0)
+                    {
+                        int timesPressed = currValue.Length; //check how many times a key has been pressed
+                        string value = currValue[0].ToString(); // to conver character to string
+                        msg += keypad[value][(timesPressed - 1) % keypad[value].Length]; // get value of key from dictionary
+                        currValue = ""; // clear current value
+                    }
+                    continue; //skips iteration
+                }
+
+                if (key == '*') // tp de;ete
+                {
+                    // Process any pending key press before deleting
+                    if (currValue.Length > 0 && currValue[0] != '*')
+                    {
+                        int timesPressed = currValue.Length;
+                        string value = currValue[0].ToString();
+                        msg += keypad[value][(timesPressed - 1) % keypad[value].Length];
+                        currValue = "";
+                    }
+
+                    if (msg.Length > 0) 
+                    {
+                        msg = msg.Remove(msg.Length - 1); //delete last value of output
+                    }
+                    continue;
+                }
+
+                if (key == '#') // break to finish
+                {
+                    if (currValue.Length > 0)
+                    {
+                        int timesPressed = currValue.Length;
+                        string value = currValue[0].ToString();
+                        msg += keypad[value][(timesPressed - 1) % keypad[value].Length];
+                        currValue = "";
+                    }
+                    break;
+                }
+
+                if (currValue.Length > 0 && currValue[0] != key) //check if there is a value and is current key is different to last key
+                {
+                    int timesPressed = currValue.Length;
+                    string value = currValue[0].ToString();
+                    msg += keypad[value][(timesPressed - 1) % keypad[value].Length];
+                    currValue = ""; 
+                }
+
+                currValue += key;
+            }
+            return msg.ToUpper(); // to return result with upper letter
+        }
+
+    }
+}
+
---
+++ CodingChallengeOldPhonePad/README.md
@@ -0,0 +1,21 @@
+# C# coding challenge - old phone keypad
+
+## Description: 
+- This project simulates the keypad of an old phone. Based on an input of numbers (key presses), 
+it will convert and print the corresponding characters/letters.
+- Build with: .NET console project
+
+## Getting started 
+
+### Prerequisites: 
+- .NET SDK (version 5.0 or higher)
+- Visual Studio (or any IDE that supports C# development)
+- Git (for cloning the repository
+
+### Installation: 
+- Clone the repository to your local machine:
+- Navigate into the project folder.
+- If you're using Visual Studio, open the project by double-clicking the .csproj file.
+
+## Contact 
+Email: leomasis95@gmail.com
